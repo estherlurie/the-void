289550d56b955cee64fd14ba837e4175748e3d43
Danielle Lurie <dlurie24@gmail.com>
2024-03-31 16:51:25 -0400

    new migration

---
+++ prisma/migrations/20240331204226_v1/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER,
+    "text" TEXT,
+    "create_time" TIMESTAMP(6) DEFAULT timezone('utc'::text, now()),
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
